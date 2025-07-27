62bdb881df8aded54cc4da20bec0699aa1824300
Rob Janssen <rob.janssen1997@gmail.com>
2025-07-27 09:25:47 +0200

    Carriers

--- src/MyParcelApi.Net/Models/Carrier.cs
+++ src/MyParcelApi.Net/Models/Carrier.cs
@@ -5,11 +5,13 @@
         None = 0,
         PostNl = 1,
         BPost = 2,
+        CheapCargo = 3,
         DPD = 4,
         DHLForYou = 9,
         DHLParcelConnect = 10,
         DHLEuroplus = 11,
         UPSStandard = 12,
         UPSExpressSaver = 13,
+        GLS = 14,
     }
 }
